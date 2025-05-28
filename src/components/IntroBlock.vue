76d49ff7bddb5920e0d5ede0bc8fdcc51b3d9b83
stevegreen4 <greensteve580@gmail.com>
2025-05-27 21:30:27 -0400

    Cleaned up my intro block

--- src/components/IntroBlock.vue
+++ src/components/IntroBlock.vue
@@ -1,12 +1,14 @@
 <script setup>
-
+import profilePicture from '../assets/profile-picture.png'
 </script>
 
 <template>
     <div class="intro-container">
-        <img src="" alt="Good picture of myself">
+        <img :src="profilePicture" alt="Good picture of myself">
         <p>
-            Brief intro about me/ elevator pitch
+            Hello, and welcome to my site. Everything you see here was crafted by me using my skills as a 
+            software developer. Please click around and learn more about all of my skills and expertise. 
+            Once your done, reach out to me on linkedIn and we can talk about how I will be a great asset to your team.
         </p>
     </div>
     
@@ -15,5 +17,20 @@
 <style scoped>
 .intro-container {
     margin-top: 100px;
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+}
+
+.intro-container img {
+    width: 150px;
+    height: 150px;
+    object-fit: cover;
+    border-radius: 50%;  
+    border: 2px solid #e0e0e0;  
+}
+
+.intro-container p {
+    line-height: 1.6;
 }
 </style>
